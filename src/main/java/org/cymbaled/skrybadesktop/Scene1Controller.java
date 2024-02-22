cb4437a37f18ac7237f37d2819dcaedda621747c
hvk1444 <shadowteam99@gmail.com>
2024-02-22 19:59:52 +0100

    checkbox

--- src/main/java/org/cymbaled/skrybadesktop/Scene1Controller.java
+++ src/main/java/org/cymbaled/skrybadesktop/Scene1Controller.java
@@ -2,32 +2,22 @@ package org.cymbaled.skrybadesktop;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class Scene1Controller {
 
     @FXML
-    private Button myButton;
-    @FXML
     private Label myLabel;
     @FXML
-    private TextField myTextField;
+    private CheckBox checkbox;
 
-    int age;
-
-    public void submit(ActionEvent event) {
-        try {
-            age = Integer.parseInt(myTextField.getText());
-            System.out.println(age);
-            if (age >= 18) {
-                myLabel.setText("You are now signed up");
-            } else {
-                myLabel.setText("You are too young");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println(e);
+    public void onSwitch(ActionEvent e) {
+        if(checkbox.isSelected()) {
+            myLabel.setText("ON");
+        }
+        if(!checkbox.isSelected()) {
+            myLabel.setText("OFF");
         }
     }
 
